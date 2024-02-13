68f9c334d0eb9d2a0dfc98dfb1fd99e811695e01
shreyagupta30 <shreyagupta3011@gmail.com>
2024-02-13 17:51:55 -0500

    change self.link to id

--- main.tf
+++ main.tf
@@ -14,20 +14,20 @@ resource "google_compute_subnetwork" "webapp_subnet" {
     name          = "webapp"
     ip_cidr_range = "10.0.0.0/24"
     region        = var.region
-    network       = google_compute_network.vpc_network.self_link
+    network       = google_compute_network.vpc_network.id
 }
 
 resource "google_compute_subnetwork" "db_subnet" {
     name          = "db"
     ip_cidr_range = "10.0.1.0/24"
     region        = var.region
-    network       = google_compute_network.vpc_network.self_link
+    network       = google_compute_network.vpc_network.id
 }
 
 resource "google_compute_route" "webapp_route" {
     name        = "webapp-route"
     dest_range  = "0.0.0.0/0"
-    network     = google_compute_network.vpc_network.self_link
+    network     = google_compute_network.vpc_network.id
     next_hop_gateway = "default-internet-gateway"
     tags        = ["webapp"]
 }
