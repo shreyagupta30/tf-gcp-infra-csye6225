bd8519e663333b619f0f7b0be736025e3726d247
shreyagupta30 <shreyagupta3011@gmail.com>
2024-02-14 15:15:44 -0500

    github actions

--- var.tf
+++ var.tf
@@ -1,9 +1,9 @@
 variable "project_id" {
-    default = "lateral-scion-414218"
+  default = "lateral-scion-414218"
 }
 variable "region" {
-    default = "us-east4"
+  default = "us-east4"
 }
 variable "vpc_name" {
-    default = "csye-tf"
+  default = "csye-tf"
 }
