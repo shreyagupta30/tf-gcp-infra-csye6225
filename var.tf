9f62184841d76b28bcf251fd68aa97c5cfe4a46d
shreyagupta30 <shreyagupta3011@gmail.com>
2024-02-14 15:29:07 -0500

    github actions check

--- var.tf
+++ var.tf
@@ -7,3 +7,23 @@ variable "region" {
 variable "vpc_name" {
   default = "csye-tf"
 }
+
+variable "ip_cir_range_webapp" {
+  default = "10.0.0.0/24"
+}
+
+variable "ip_cir_range_db" {
+  default = "10.0.1.0/24"
+}
+
+variable "webapp_subnet_name" {
+  default = "webapp-route"
+}
+
+variable "webapp_route_range" {
+  default = "0.0.0.0/0"
+}
+
+variable "webapp_route_tags" {
+  default = "webapp"
+}
